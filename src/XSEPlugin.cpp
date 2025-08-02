ae26a72784cb080cade27010c4ca4e992f0d53fe
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2025-08-02 10:27:50 +0100

    fixes

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -7,7 +7,7 @@ void MessageHandler(SKSE::MessagingInterface::Message *a_msg)
 	case SKSE::MessagingInterface::kDataLoaded:
 		Events_Space::animEventHandler::Register(true, false);
 		Events_Space::Events::install();
-		Events_Space::HitEventHandler::InstallHooks();
+		Events_Space::HitEventHandler::InstallMageHooks();
 		Events_Space::Settings::GetSingleton()->Load();
 		break;
 
@@ -30,4 +30,5 @@ void Init()
 
 void Load(){
 	Events_Space::Events::install_protected();
+	Events_Space::HitEventHandler::InstallHooks();
 }
