7cb1adcfb05ab3416a52bc760a3d663ff88ad410
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2025-08-03 00:56:24 +0100

    Update PCH.h

--- include/PCH.h
+++ include/PCH.h
@@ -34,7 +34,6 @@ namespace stl
 	template <class T>
 	void write_thunk_call(std::uintptr_t a_src)
 	{
-		SKSE::AllocTrampoline(14);
 		auto& trampoline = SKSE::GetTrampoline();
 		T::func = trampoline.write_call<5>(a_src, T::thunk);
 	}
@@ -56,8 +55,6 @@ namespace stl
 	template <class T>
 	void write_thunk_jump(std::uintptr_t a_src)
 	{
-		SKSE::AllocTrampoline(14);
-
 		auto& trampoline = SKSE::GetTrampoline();
 		T::func = trampoline.write_branch<5>(a_src, T::thunk);
 	}
